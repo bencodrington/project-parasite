b2d4764b4e67bc50e24e6ba09b62d3cfb29e1230
Ben <benvenda@gmail.com>
2018-10-28 23:37:47 -0400

    Make physics entities only spawn on clients with authority - Add jumping

--- Project Parasite/Assets/PhysicsEntity.cs
+++ Project Parasite/Assets/PhysicsEntity.cs
@@ -1,39 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PhysicsEntity : MonoBehaviour {
-
-	float gravityAcceleration = -1f;
-	const float MAX_SPEED = 5f;
-	float velocityX = 0f;
-	float velocityY = 0f;
+public class PhysicsEntity {
+	Transform transform;
 
 	float height = 0.5f;
 
+	float gravityAcceleration = -1f;
+	float maxSpeed = 5f;
+	float velocityX = 0f;
+	float velocityY = 0f;
+
 	bool isOnGround = false;
 
-	// Update is called once per frame
-	void Update () {
+	public PhysicsEntity(Transform transform, float height = 0.5f, float gravity = -1f, float maxSpeed = 5f) {
+		this.transform = transform;
+		this.height = height;
+		this.gravityAcceleration = gravity; 
+	}
+
+	public void Update () {
 		float obstacleHeight;
-		velocityY = Mathf.Clamp(velocityY + gravityAcceleration * Time.deltaTime, -MAX_SPEED, MAX_SPEED);
+		velocityY = Mathf.Clamp(velocityY + gravityAcceleration * Time.deltaTime, -maxSpeed, maxSpeed);
 		// Check for collisions at new location
 		// If pixel below entity overlaps an obstacle
-		Vector2 newPos = new Vector2(transform.position.x, transform.position.y + velocityY);
-		Collider2D obstacle = Physics2D.OverlapPoint(newPos + new Vector2(0, -height));
+		Vector2 newPosition = new Vector2(transform.position.x + velocityX, transform.position.y + velocityY);
+		Collider2D obstacle = Physics2D.OverlapPoint(newPosition + new Vector2(0, -height));
 		if (obstacle != null) {
 			// Entity is touching the ground
 			isOnGround = true;
 			// Then new velocityY is 0 and position is directly above the obstacle
 			velocityY = 0;
 			obstacleHeight = obstacle.transform.localScale.y / 2;
-			newPos.y = obstacle.transform.position.y + obstacleHeight + height;
+			newPosition.y = obstacle.transform.position.y + obstacleHeight + height;
 		} else {
 			// Nothing is below entity
 			isOnGround = false;
 		}
 		// TODO: if velocity is above a certain threshold (probably just under half of width, assuming it's smaller than height)
 		// 	check for collisions in a line between position & new position
-		transform.position = newPos;
+		transform.position = newPosition;
+	}
+
+	public void AddVelocity(float x, float y) {
+		velocityX += x;
+		velocityY += y;
 	}
 }
--- Project Parasite/Assets/PlayerCharacter.cs
+++ Project Parasite/Assets/PlayerCharacter.cs
@@ -8,7 +8,10 @@ public class PlayerCharacter : NetworkBehaviour {
 	private SpriteRenderer spriteRenderer;
 	private Color colour;
 
+	private PhysicsEntity physicsEntity;
+
 	float movementSpeed = 10f;
+	float jumpVelocity = .25f;
 
 	[SyncVar]
 	Vector3 serverPosition;
@@ -17,42 +20,35 @@ public class PlayerCharacter : NetworkBehaviour {
 	// Use this for initialization
 	void Start () {
 		spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+		RpcGeneratePhysicsEntity();
 	}
 	
 	void Update () {
 		// Called once per frame for each PlayerCharacter
 		if (hasAuthority) {
 			HandleInput();
+			if (physicsEntity != null) {
+				physicsEntity.Update();
+			}
 		} else {
 			// Verify current position is up to date with server position
 			transform.position = Vector3.SmoothDamp(transform.position, serverPosition, ref serverPositionSmoothVelocity, 0.25f);
 		}
-
-		// if (Input.GetKeyDown(KeyCode.W)) {
-		// 	this.transform.Translate(0, 1, 0);
-		// }
-		// if (Input.GetKeyDown(KeyCode.A)) {
-		// 	this.transform.Translate(-1, 0, 0);
-		// }
-		// if (Input.GetKeyDown(KeyCode.S)) {
-		// 	this.transform.Translate(0, -1, 0);
-		// }
-		// if (Input.GetKeyDown(KeyCode.D)) {
-		// 	this.transform.Translate(1, 0, 0);
-		// }
-
 
 	}
 
 	void HandleInput() {
 		// Movement
 		float movementX = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+		// TODO: check if on ground
+		if (Input.GetKeyDown(KeyCode.W)) {
+			physicsEntity.AddVelocity(0, jumpVelocity);
+		}
 		// Has authority, so translate immediately
 		transform.Translate(movementX, 0, 0);
 		// Update the server's position
 		// TODO: clump these updates to improve network usage?
 		CmdUpdatePosition(transform.position);
-
 	}
 
 	// COMMANDS
@@ -67,8 +63,11 @@ public class PlayerCharacter : NetworkBehaviour {
 	// CLIENTRPC
 
 	[ClientRpc]
-	void RpcOnColourChange(Color colour) {
-		spriteRenderer.color = colour;
+	void RpcGeneratePhysicsEntity() {
+		if (hasAuthority) {
+			// Add physics entity
+			physicsEntity = new PhysicsEntity(transform);
+		}
 	}
 
 	[ClientRpc]
