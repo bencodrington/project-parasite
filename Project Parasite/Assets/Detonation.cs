b9394048dbced1a1f8211e3b8af2ffec18b3b981
Ben <benvenda@gmail.com>
2018-11-04 15:49:39 -0500

    Hunter shots now detonate (and draw circle to show it)

---
+++ Project Parasite/Assets/Detonation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detonation : MonoBehaviour {
+
+	public SpriteRenderer sR;
+	public float lifeTime = 0.1f;
+
+	void Update() {
+		lifeTime -= Time.deltaTime;
+		if (lifeTime <= 0) {
+			Destroy(gameObject);
+		}
+		float alpha = Random.Range(0f, 1f);
+		sR.color = new Color(sR.color.r, sR.color.g, sR.color.b, alpha);
+	}
+
+
+}
--- Project Parasite/Assets/Scripts/Characters/Hunter.cs
+++ Project Parasite/Assets/Scripts/Characters/Hunter.cs
@@ -7,15 +7,22 @@ public class Hunter : Character {
 
 	// The time it takes to fully scan an NPC
 	const float SCAN_TIME = 0.75f;
+	const float DETONATION_RADIUS = 1.5f;
+
+	public GameObject detonationPrefab;
 
 	protected override void HandleInput()  {
+		Vector2 mousePosition;
 		// Movement
 		HandleHorizontalMovement();
 		// Attack
 		if (Input.GetMouseButtonDown(0)) {
-			Collider2D target = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition), characterLayerMask);
+			mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D target = Physics2D.OverlapPoint(mousePosition, characterLayerMask);
 			if (target != null) {
 				CmdAttackTarget(target.transform.parent.GetComponent<NetworkIdentity>().netId);
+			} else {
+				CmdAttackPoint(mousePosition);
 			}
 		}
 		// Scan NPC
@@ -38,17 +45,33 @@ public class Hunter : Character {
 	void CmdAttackTarget(NetworkInstanceId targetNetId) {
 		// Find target's game object on this client
 		GameObject targetGameObject = NetworkServer.FindLocalObject(targetNetId);
-		// Get PlayerCharacter script
-		Character targetPlayerCharacter = targetGameObject.GetComponentInChildren<Character>();
-		bool isNpc = targetPlayerCharacter is NonPlayerCharacter;
-		// Inflict damage
-		if (!isNpc || (isNpc && ((NonPlayerCharacter)targetPlayerCharacter).isInfected)) {
-			// Damage parasite
-			targetPlayerCharacter.playerObject.RpcTakeDamage(25);
-		} else if (isNpc) {
-			// Instant kill npcs
-			FindObjectOfType<NpcManager>().DespawnNpc(targetNetId);
+		Vector3 targetPoint = targetGameObject.transform.position;
+		CmdAttackPoint(targetPoint);
+	}
+
+	[Command]
+	void CmdAttackPoint(Vector3 targetPoint) {
+		Character character;
+		bool isNpc;
+		// Spawn detonation object
+		RpcSpawnDetonation(targetPoint);
+		// Find all characters in radius DETONATION_RADIUS
+		Collider2D[] characterCollidersInRadius = Physics2D.OverlapCircleAll(targetPoint, DETONATION_RADIUS, characterLayerMask);
+		// For each Character
+		foreach (Collider2D characterCollider in characterCollidersInRadius) {
+			// Get Character script
+			character = characterCollider.transform.parent.GetComponentInChildren<Character>();
+			isNpc = character is NonPlayerCharacter;
+			// Inflict damage
+			if (!isNpc || (isNpc && ((NonPlayerCharacter)character).isInfected)) {
+				// Damage parasite
+				character.playerObject.RpcTakeDamage(25);
+			} else if (isNpc) {
+				// Instant kill npcs
+				FindObjectOfType<NpcManager>().DespawnNpc(character.netId);
+			}
 		}
+
 	}
 
 	[Command]
@@ -58,5 +81,12 @@ public class Hunter : Character {
 		// Get NonPlayerCharacter script
 		NonPlayerCharacter npc = npcGameObject.GetComponentInChildren<NonPlayerCharacter>();
 		npc.RpcVerify();
+	}
+
+	// ClientRpc
+
+	[ClientRpc]
+	void RpcSpawnDetonation(Vector3 detonationPosition) {
+		Instantiate(detonationPrefab, detonationPosition, Quaternion.identity);
 	}
 }
