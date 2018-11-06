afc698e4599f0e79b485cbee323d1494e32713d4
Ben <benvenda@gmail.com>
2018-11-05 20:49:48 -0500

    Add Elevator Call Fields and make them work

---
+++ Project Parasite/Assets/ElevatorCallField.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ElevatorCallField : NetworkBehaviour {
+
+	public Elevator elevator;
+	public int stopIndex;
+
+	public Vector2 size;
+
+	private int callerLayerMask;
+	private Collider2D caller;
+	
+	void Start() {
+		int hunterMask = 1 << LayerMask.NameToLayer("Hunters");
+		int npcMask = 1 << LayerMask.NameToLayer("NPCs");
+		int parasiteMask = 1 << LayerMask.NameToLayer("Parasites");
+		callerLayerMask = hunterMask + npcMask + parasiteMask;
+							
+	} 
+
+	void FixedUpdate() {
+		if (!isServer) { return; }
+		// TODO: this probably doesn't need to run every single physics update
+		caller = Physics2D.OverlapArea(transform.position,
+										transform.position + new Vector3(size.x, size.y, 0),
+										callerLayerMask);
+		Debug.DrawLine(transform.position, transform.position + new Vector3(size.x, size.y, 0));
+		if (caller != null) {
+			CallElevator();
+		}
+	}
+
+	void CallElevator() {
+		elevator.CmdCallToStop(stopIndex);
+	}
+}
