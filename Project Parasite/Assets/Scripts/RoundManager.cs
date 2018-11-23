d4fddd2e2814d781338745cc16e85c6b45c3b9ce
Ben Codrington <benvenda@gmail.com>
2018-11-22 23:50:12 -0500

    Add 'huntersOnlyMode' for easy debugging

--- Project Parasite/Assets/Scripts/RoundManager.cs
+++ Project Parasite/Assets/Scripts/RoundManager.cs
@@ -11,6 +11,8 @@ public class RoundManager : MonoBehaviour {
 	Vector2 parasiteSpawnPoint;
 	Vector2 hunterSpawnPoint;
 
+	bool huntersOnlyMode = false;
+
 	void Start () {
 		// Cache Player Objects
 		connectedPlayers = FindObjectsOfType<PlayerObject>();
@@ -24,6 +26,7 @@ public class RoundManager : MonoBehaviour {
 		Vector2 spawnPoint;
 		// Randomly select one of the players to be parasite, the rest are hunters
 		int indexOfParasite = Random.Range(0, n);
+		if (huntersOnlyMode) { indexOfParasite = -1; }
 		CharacterType characterType;
 		for (int i = 0; i < n; i++) {
 			if (i == indexOfParasite) {
