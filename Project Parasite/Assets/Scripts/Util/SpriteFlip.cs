cf3d6958333e50fc4210c90b163b557ac900e02a
Ben Codrington <benvenda@gmail.com>
2019-03-24 23:45:53 -0400

    Add Sprite flipping for hunters and parasites

---
+++ Project Parasite/Assets/Scripts/Util/SpriteFlip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlip : MonoBehaviour
+{
+    float lastX;
+
+    void Start() {
+        lastX = transform.position.x;
+    }
+
+    void Update() {
+        float newX = transform.position.x;
+        if (newX > lastX) {
+            // Moving right
+            transform.eulerAngles = Vector3.zero;
+        } else if (lastX > newX) {
+            // Moving left
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+        lastX = newX;
+    }
+}
