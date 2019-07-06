d0710d255b8479032513c1c61470cb0d53b65b39
Ben Codrington <benvenda@gmail.com>
2019-07-06 15:40:04 -0400

    Add sound effects when charging burst

--- Project Parasite/Assets/Scripts/UI/BurstIndicator.cs
+++ Project Parasite/Assets/Scripts/UI/BurstIndicator.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BurstIndicator : MonoBehaviour
 {
+    #region [Public Variables]
+    
+    public AudioClip burstChargeSound;
+    public AudioClip burstReadySound;
+    
+    #endregion
 
     #region [Private Variables]
 
@@ -13,10 +19,13 @@ public class BurstIndicator : MonoBehaviour
     SpriteRenderer spriteRenderer;
     SpriteMask spriteMask;
     Animator animator;
+    AudioSource burstChargeAudioSource;
+    AudioSource burstReadyAudioSource;
 
     float timeToFill = 0.25f;
     float timeElapsed = 0f;
     bool isFilling = false;
+    bool isFull = false;
     
     #endregion
 
@@ -30,12 +39,14 @@ public class BurstIndicator : MonoBehaviour
     public void StartFilling() {
         timeElapsed = 0f;
         isFilling = true;
+        burstChargeAudioSource.Play();
     }
 
     public void StopFilling() {
         isFilling = false;
         float progress = Mathf.Min(timeElapsed / timeToFill, 1);
         FindObjectOfType<CameraFollow>().ShakeScreen(Mathf.Lerp(0, .25f, progress), 0.2f);
+        burstChargeAudioSource.Stop();
     }
     
     #endregion
@@ -48,6 +59,8 @@ public class BurstIndicator : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         // Hide at start
         spriteRenderer.color = Color.clear;
+        burstChargeAudioSource = Utility.AddAudioSource(gameObject, burstChargeSound);
+        burstReadyAudioSource = Utility.AddAudioSource(gameObject, burstReadySound);
     }
 
     void Update() {
@@ -58,11 +71,15 @@ public class BurstIndicator : MonoBehaviour
             spriteMask.alphaCutoff = 1 - progress;
             if (timeElapsed >= timeToFill) {
                 animator.SetBool("isActive", true);
-                FindObjectOfType<CameraFollow>().ShakeScreen(0.05f, 0.1f);
+                if (!isFull) {
+                    OnReadyToBurst();
+                }
+                isFull = true;
             } else {
                 FindObjectOfType<CameraFollow>().ShakeScreen(Mathf.Lerp(0, 0.03f, progress), 0.1f);
             }
         } else {
+            isFull = false;
             spriteRenderer.color = Color.clear;
             spriteMask.alphaCutoff = 0;
                 animator.SetBool("isActive", false);
@@ -70,4 +87,13 @@ public class BurstIndicator : MonoBehaviour
     }
     
     #endregion
+
+    #region [Private Methods]
+    
+    void OnReadyToBurst() {
+        burstReadyAudioSource.Play();
+    }
+    
+    #endregion
+
 }
---
+++ Project Parasite/Assets/Scripts/Util/AnimationEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class AnimationEventHandler: MonoBehaviour
+{
+    #region [Public Variables]
+    
+    public AudioClip clip;
+    
+    #endregion
+
+    #region [Private Variables]
+    
+    AudioSource source;
+    
+    #endregion
+
+    #region [MonoBehaviour Callbacks]
+    
+    void Start() {
+        source = Utility.AddAudioSource(gameObject, clip);
+    }
+    
+    #endregion
+
+    #region [Private Methods]
+    
+    void PlaySound() {
+        source.Play();
+        // CLEANUP:
+        FindObjectOfType<CameraFollow>().ShakeScreen(0.07f, 0.2f);
+    }
+    
+    #endregion
+
+}
