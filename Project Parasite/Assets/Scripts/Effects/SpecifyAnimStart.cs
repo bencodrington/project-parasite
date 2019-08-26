2a6c878f71f1d602b7931f94c8a781220ad1d0f6
Ben Codrington <benvenda@gmail.com>
2019-08-25 20:57:07 -0400

    Add engine room environment art

---
+++ Project Parasite/Assets/Scripts/Effects/SpecifyAnimStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecifyAnimStart : MonoBehaviour
+{
+    #region [Public Variables]
+    
+    [Range(0, 1)]
+    public float normalizedTime;
+    public string animationStateName;
+    
+    #endregion
+
+    #region [MonoBehaviour Callbacks]
+    
+    void Start() {
+        GetComponent<Animator>().Play(animationStateName, 0, normalizedTime);
+    }
+    
+    #endregion
+}
