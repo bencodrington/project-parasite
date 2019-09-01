adaaf33ef40dc05947245c66ee44ad956453c544
Ben Codrington <benvenda@gmail.com>
2019-09-01 18:00:33 -0400

    Add intro music

---
+++ Project Parasite/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    #region [Public Variables]
+    
+    public AudioClip introSong;
+    
+    #endregion
+
+    #region [Private Variables]
+    
+    AudioSource audioSource;
+    
+    #endregion
+    
+    #region [MonoBehaviour Callbacks]
+    
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = introSong;
+        audioSource.Play();
+    }
+    
+    #endregion
+}
