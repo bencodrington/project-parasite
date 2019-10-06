f9411dc5acc9e7d6cd8b8aa3998fdd2e9065f60b
Ben Codrington <benvenda@gmail.com>
2019-10-06 16:54:59 -0400

    Play sounds when parasite walks

---
+++ Project Parasite/Assets/Scripts/Util/RandomSoundSet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundSet : MonoBehaviour
+{
+    #region [Public Variables]
+    
+    public AudioClip[] sounds;
+    
+    #endregion
+
+    #region [Private Variables]
+    
+    AudioSource audioSource;
+    
+    #endregion
+
+    #region [Public Methods]
+    
+    void PlayRandom() {
+        audioSource.clip = sounds[Random.Range(0, sounds.Length - 1)];
+        audioSource.Play();
+    }
+    
+    #endregion
+
+    #region [MonoBehaviour Callbacks]
+    
+    void Start() {
+        audioSource = Utility.AddAudioSource(gameObject);
+    }
+    
+    #endregion
+}
