fd41baf5958bd09997c2e98a192f8087eb5cb438
Ben Codrington <benvenda@gmail.com>
2019-11-18 19:49:47 -0500

    Add pounce charge sound

--- Project Parasite/Assets/Scripts/PounceIndicator.cs
+++ Project Parasite/Assets/Scripts/PounceIndicator.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class PounceIndicator : MonoBehaviour {
 
+	#region [Public Variables]
+	
+	public AudioClip pounceChargeSound;
+	
+	#endregion
+
+	#region [Private Variables]
+	
 	Vector2 minSize = new Vector2(1, 1);
 	Vector2 maxSize = new Vector2(4f, 4f);
 
 	SpriteRenderer spriteRenderer;
-	void Start() {
-		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-		Hide();
-	}
+	AudioSource pounceChargeAudioSource;
+	
+	#endregion
+
+	#region [Public Methods]
 
 	public void Hide() {
 		SetAlpha(0);
+		pounceChargeAudioSource.Stop();
 	}
 	public void Show() {
 		SetAlpha(1);
+		pounceChargeAudioSource.Play();
 	}
 
 	// Percentage should be a value in the range [0..1]
@@ -25,6 +36,27 @@ public class PounceIndicator : MonoBehaviour {
 		SetSize(percentage);
 		SetAlpha(percentage);
 	}
+
+	// Angle should be in degrees
+	public void SetAngle(float angle) {
+		spriteRenderer.transform.eulerAngles = new Vector3(0, 0, 1) * angle;
+	}
+	
+	#endregion
+
+	#region [MonoBehaviour Callbacks]
+	
+	void Start() {
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		pounceChargeAudioSource	= AudioManager.AddAudioSource(gameObject, pounceChargeSound, 1f, true, AudioManager.Instance.sfxGroup);
+
+		Hide();
+	}
+	
+	#endregion
+
+	#region [Private Methods]
+	
 	void SetSize(float percentage) {
 		spriteRenderer.transform.localScale = Vector2.Lerp(minSize, maxSize, percentage);
 	}
@@ -33,9 +65,7 @@ public class PounceIndicator : MonoBehaviour {
 		Color c = spriteRenderer.color;
 		spriteRenderer.color = new Color(c.r, c.g, c.b, a);
 	}
+	
+	#endregion
 
-	// Angle should be in degrees
-	public void SetAngle(float angle) {
-		spriteRenderer.transform.eulerAngles = new Vector3(0, 0, 1) * angle;
-	}
 }
