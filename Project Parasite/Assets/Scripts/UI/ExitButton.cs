74860fce11736b7ac0428fe7188d0b02c05bc85c
Ben Codrington <benvenda@gmail.com>
2019-11-21 16:46:25 -0500

    Add 'exit game' button to main menu

---
+++ Project Parasite/Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    
+    #region [Public Methods]
+    
+    public void OnClick() {
+        Application.Quit();
+    }
+    
+    #endregion
+
+}
