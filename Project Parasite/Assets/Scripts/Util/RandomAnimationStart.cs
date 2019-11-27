58fa2b32e952ad5825a4096f6b9791b672a698d1
Ben Codrington <benvenda@gmail.com>
2019-11-26 19:31:46 -0500

    Add floating tables and chairs - animate them - make them start at a random point in the animation

---
+++ Project Parasite/Assets/Scripts/Util/RandomAnimationStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationStart : MonoBehaviour
+{
+    
+    #region [MonoBehaviour Callbacks]
+    
+    void Start() {
+        Animator animator = GetComponent<Animator>();
+        // Skip to a random point in the animation
+        animator.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, -1, Random.Range(0f, 1f));
+    }
+    
+    #endregion
+
+}
