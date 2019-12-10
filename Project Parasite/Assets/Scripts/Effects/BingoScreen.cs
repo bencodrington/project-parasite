a3bc1aa05f8daabc4c4a16b58be6904aae42a053
Ben Codrington <benvenda@gmail.com>
2019-12-09 19:11:53 -0500

    Update Bingo Room - Add screens - Tweak windows and layout - Add depth

---
+++ Project Parasite/Assets/Scripts/Effects/BingoScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BingoScreen : MonoBehaviour
+{
+
+    #region [Public Variables]
+    
+    public TextMeshPro leftText;
+    public TextMeshPro centerText;
+    public TextMeshPro rightText;
+    
+    #endregion
+
+    #region [Private Variables]
+    
+    string[] PHRASES = new string[]{
+        "it's bingo time",
+        "grab your card",
+        "crave that rush",
+        "ohana means bingo",
+        "yeah baby yeah",
+        "let's go bingo",
+        "real spicy bingo",
+        "sweet bingo meat",
+        "hit that bingo"
+    };
+
+    int currentPhraseIndex = -1;
+    
+    #endregion
+
+    #region [Public Methods]
+    
+    public void NewPhrase() {
+        // Select new phrase
+        // int newPhraseIndex = currentPhraseIndex; // For testing word length
+        int newPhraseIndex = Random.Range(0, PHRASES.Length);
+        if (newPhraseIndex == currentPhraseIndex) {
+            // Ensure we don't show the same phrase twice in a row
+            newPhraseIndex = newPhraseIndex == PHRASES.Length - 1 ? 0 : newPhraseIndex + 1;
+        }
+        currentPhraseIndex = newPhraseIndex;
+        string[] newWords = PHRASES[currentPhraseIndex].Split(' ');
+        if (newWords.Length != 3) {
+            Debug.LogError("BingoScreen:NewPhrase(): '" + PHRASES[currentPhraseIndex] + "' isn't exactly 3 words.");
+            return;
+        }
+        // Update textMeshPro components
+        leftText.text   = newWords[0];
+        centerText.text = newWords[1];
+        rightText.text  = newWords[2];
+    }
+    
+    #endregion
+}
---
+++ Project Parasite/Assets/Scripts/Effects/ScreenFlicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFlicker : MonoBehaviour
+{
+    
+    #region [Public Variables]
+    
+    public SpriteRenderer spriteRenderer;
+    
+    #endregion
+
+    #region [Private Variables]
+
+    const float minOpacity = 0.6f;
+    const float maxOpacity = 1f;
+    const float timeBetweenFlickers = 0.05f;
+    float timeToNextFlicker;
+    
+    Color startColour;
+    
+    #endregion
+
+    #region [MonoBehaviour Callbacks]
+
+    void Start() {
+        startColour = spriteRenderer.color;
+        timeToNextFlicker = timeBetweenFlickers;
+    }
+    
+    void Update() {
+        if (timeToNextFlicker < 0) {
+            spriteRenderer.color = new Color(startColour.r, startColour.g, startColour.b, Random.Range(minOpacity, maxOpacity));
+            timeToNextFlicker += timeBetweenFlickers;
+        }
+        timeToNextFlicker -= Time.deltaTime;
+    }
+    
+    #endregion
+    
+}
