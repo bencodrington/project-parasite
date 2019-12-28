ebc6e29a818dd4c5878b6f4e1c11d99371c4cabb
Ben Codrington <benvenda@gmail.com>
2019-12-28 14:25:01 -0500

    Add "waiting for players/game leader" text to lobby

---
+++ Project Parasite/Assets/Scripts/UI/WaitingFor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class WaitingFor : MonoBehaviour
+{
+
+    #region [Private Variables]
+
+    string CLIENT_TEXT = "Waiting for Group Leader...";
+    string LEADER_TEXT = "Waiting for players...";
+
+    
+    TextMeshProUGUI text;
+    
+    #endregion
+
+    #region [MonoBehaviour Callbacks]
+
+    void OnEnable() {
+        text = GetComponent<TextMeshProUGUI>();
+        if (text == null) { return; }
+        if (PhotonNetwork.IsMasterClient) {
+            text.text = LEADER_TEXT;
+        } else {
+            text.text = CLIENT_TEXT;
+        }
+    }
+
+    #endregion
+
+}
